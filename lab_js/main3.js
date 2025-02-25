e9a8144e5ea9229958ecf691951f8a8b0bc70961
ILNAZ-linux <sabirov.main@gmail.com>
2025-02-25 19:24:09 +0300

    исправил ошибку

--- lab_js/main3.js
+++ lab_js/main3.js
@@ -14,7 +14,7 @@ function sort(){
     do{
         flag = false;
         for(let i = 0; i < n - 1; i++ ){
-            if(arr[i] > arr[i + 1]){
+            if(arr[i] < arr[i + 1]){
                 [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
                 flag = true;
             }
